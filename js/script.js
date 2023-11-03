b367f52721f7da7d76d7a53987bcd520e9cd40e8
Branden108 <bsi@hawk.iit.edu>
2023-11-03 13:17:06 -0500

    Adding JavaScript Code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 function init(){
-//add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+    var entry = document.getElementById('entry');
+    var output = document.getElementById('output').innerHTML = entry.value;
+function myEventAlert{
+	var input = document.getElementByID('input').value;
+    alert('Branden Si: ' + input.value);
+	console.log('ALERT');
+	console.log('input: ' + input);
+	console.log('output: ' + output):
+	document.getElementById('output').innerHTML = input;
+	console.log('New output: ' + output);
+  }
+	button.addEventListener('click', myEventAlert);
+}
 window.addEventListener('load', init);
